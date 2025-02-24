8ff1e20c7f94c1e9e5fbb2dfe7eb9fd70aa29d0a
landry5 <nsengilandry5@gmail.com>
2025-02-24 14:32:34 +0000

    Initial commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() { 
   return (
       <View style={styles.container}> 
-      <Text>Hello there from expo router</Text>
+      <Text>Hello world!</Text>
       </View>
   );
 
